2a5e386f0fce1db67621648b64c2758de6b57c79
meixz <meixz@mail.ru>
2025-03-28 01:09:40 +0300

    10: bubble sort

---
+++ Algorithms&data_structures/BubbleSort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    for j in range(len(arr)):
+        for i in range(0, len(arr) - j - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+    return arr
+
+
+# Пример использования:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(my_list)
+print("Отсортированный список:", my_list)
