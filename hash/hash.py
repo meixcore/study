68bf5dfb1362bd468b72b40d9621a38ba24fba66
meix <meixz@mail.ru>
2026-09-02 11:49:03 +0300

    hash v2

--- hash/hash.py
+++ hash/hash.py
@@ -4,6 +4,7 @@ import base64
 import hmac
 
 class HashPasswordClient:
+    algorithm = 'sha256'
     iteration_count = 100_000
     salt_length = 16
 
@@ -14,7 +15,7 @@ class HashPasswordClient:
 
         password_bytes = raw_password.encode("utf-8")
         hash_bytes = hashlib.pbkdf2_hmac(
-            hash_name="sha256",
+            hash_name=self.algorithm,
             password=password_bytes,
             salt=salt,
             iterations=self.iteration_count,
@@ -23,33 +24,38 @@ class HashPasswordClient:
         salt_b64 = base64.b64encode(salt).decode("utf-8")
         hash_b64 = base64.b64encode(hash_bytes).decode("utf-8")
 
-        return f"{salt_b64}${hash_b64}"
+        return f"{self.algorithm}${self.iteration_count}${salt_b64}${hash_b64}"
 
     def validate_password(self, input_password: str, hashed_password: str) -> bool:
         # принимает на вход 2 строки с введенным паролем и хеш-суммой пароля,
         # сравнивает их и возвращает тру если сумма от input_password
         # соответствует hashed_password
-        salt_b64, hash_b64 = hashed_password.split("$")
-        salt = base64.b64decode(salt_b64)
-        original_hash = base64.b64decode(hash_b64)
+        try:
+            algorithm, iterations, salt_b64, hash_b64 = hashed_password.split("$")
+            iterations = int(iterations)
+            salt = base64.b64decode(salt_b64)
+            original_hash = base64.b64decode(hash_b64)
+        except (ValueError, TypeError):
+            return False
 
         password_bytes = input_password.encode("utf-8")
         new_hash = hashlib.pbkdf2_hmac(
-            "sha256",
+            algorithm,
             password_bytes,
             salt,
-            self.iteration_count,
+            iterations,
         )
 
         return hmac.compare_digest(new_hash, original_hash)
 
-client = HashPasswordClient()
-password = "qwerty123"
+if __name__ == "__main__":
+    client = HashPasswordClient()
+    password = "qwerty123"
 
-hash1 = client.hash_password(password)
-hash2 = client.hash_password(password)
+    hash1 = client.hash_password(password)
+    hash2 = client.hash_password(password)
 
-print(hash1)
-print(hash2)
-print(client.validate_password(password, hash1))
-print(client.validate_password(password, hash2))
+    print(hash1)
+    print(hash2)
+    print(client.validate_password(password, hash1))
+    print(client.validate_password(password, hash2))
